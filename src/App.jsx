c158bbaa17d5c613bad8c0890beac5dbfd4efa87
Vijay Kumar <vijaykumar_23se170@dtu.ac.in>
2024-10-10 23:12:08 +0530

    first com

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div></div>
+    <div>
+     This is CodeDiaries
+    </div>
   )
 }
 
