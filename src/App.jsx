3c1b1ea32df7e6da14b4c8661ebd2c2516fe3eae
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-13 00:26:26 +0530

    started working on forms and also added jodit-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { register, handleSubmit } = useForm();
+  const OnSubmit = (data)=>{
+    console.log(data);
+  }
+  
   return (
    <>
     <div>
       <h1 className='font-bold text-8xl flex mx-24'>Create Post</h1>
+      <form onSubmit={handleSubmit(OnSubmit)}>
+        <input {...register("Title of the Post")} />
+      </form>
     </div>
    </>
   )
