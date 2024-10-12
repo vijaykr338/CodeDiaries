e190422f6a43aff982bd9da704f0f92d0deedf0b
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-13 00:18:40 +0530

    installed hook-forms & started working on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div></div>
+   <>
+    <div>
+      <h1 className='font-bold text-8xl flex mx-24'>Create Post</h1>
+    </div>
+   </>
   )
 }
 
