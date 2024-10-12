ff9aa2dfdbbbef5fcf26e04f29560a989e1165f6
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-13 00:28:58 +0530

    made a basic form layout

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ function App() {
       <h1 className='font-bold text-8xl flex mx-24'>Create Post</h1>
       <form onSubmit={handleSubmit(OnSubmit)}>
         <input {...register("Title of the Post")} />
+        <input {...register("Tags")} />
+        <input {...register("Content")} />
+        <input type="submit" value="" />
       </form>
     </div>
    </>
