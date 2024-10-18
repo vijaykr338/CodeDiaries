01b626a2f660c6de6b6946009a5ad9a8fe9f63a0
Vijay Kumar <vijaykumar_23se170@dtu.ac.in>
2024-10-18 23:43:06 +0530

    all components merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 
 import { useState } from 'react'
-import SignUp from './Components/Authentication/SignUp'
-import SignIn from './Components/Authentication/SignIn'
+import SignUp from './components/Authentication/SignUp'
+import SignIn from './components/Authentication/SignIn'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import PostPage from './components/PostCreation/PostPage';
+import Edit from './components/PostCreation/Edit';
 
 function App() {
   return (
   <Router>
     <Routes>
-    <Route path="/" element={<SignIn />} />
+    <Route path="/" element={<HomePage />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/signin" element={<SignIn />} />
+    <Route path="/post/example" element={<PostPage/>} /> 
+    <Route path="/create-post" element={<Edit/>} /> 
+    
+  
     </Routes>
   </Router>
-
+  )
+  }
 
 export default App
 
--- src/components/PostCreation/Edit.jsx
+++ src/components/PostCreation/Edit.jsx
@@ -14,26 +14,27 @@ function Edit() {
    
    return (
       <>
-         <div className="flex justify-center flex-shrink-0 bg-slate-300 h-full w-full">
+         <div className="flex justify-center flex-shrink-0 bg-black h-full w-full">
             <div className="flex w-11/12 sm:w-4/5 bg-white my-10 px-10 h-full py-6 flex-col rounded-lg">
                <div className="flex font-bold text-4xl sm:text-7xl">Create Post!</div>
                <div className="flex flex-col flex-shrink-0 sm:w-3/5 justify-start gap-6 my-7 sm:text-2xl">
                   <form className="flex flex-col gap-9" onSubmit={handleSubmit(OnSubmit)}>
-                     <input className="border-2 border-black rounded-md px-6 py-3 font-bold placeholder:text-black"
+                     <input className="border-2 border-black rounded-md px-6 py-3  placeholder:text-black"
                      type="text" placeholder="Title for the Post" {...register("Title")}/>
 
-                     <input className="border-2 border-black rounded-md px-6 py-3 font-bold placeholder:text-black"
+                     <input className="border-2 border-black rounded-md px-6 py-3  placeholder:text-black"
                      type="url" placeholder="Image URL" {...register("img_url")}/>
 
-                     <input className="border-2 border-black rounded-md px-6 py-3 font-bold placeholder:text-black"
+                     <input className="border-2 border-black rounded-md px-6 py-3  placeholder:text-black"
                      type="text" placeholder="Tags" {...register("Tags")}/>
                      
                      <JoditEditor className="w-96" ref={editor} value={Content} onChange={newcontent => {
                         setContent(newcontent)
                      }} />
-
-                    <input type="submit" className="border-2 border-black bg-black font-bold text-white py-2 px-2 w-28 rounded-full hover:shadow-xl shadow-cyan-300"/>
-
+<input 
+  type="submit" 
+  className="border-2 border-black bg-black font-bold text-white py-2 px-3  rounded-full hover:shadow-xl shadow-cyan-300"
+/>
                     <input
                     className="rounded-large"
                     type="textarea" value={Content} {...register("content(html)")} />
