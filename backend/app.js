aa3e5be828203d46c7c57947ff87d6b36183bab9
Vijay Kumar <vijaykumar_23se170@dtu.ac.in>
2024-11-02 13:15:24 +0530

    attempt to fix vercel

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://code-diaries.vercel.app/",
     credentials: true,
   })  
 );  
